066a9f3c56678f10e1809e27261f490eb301b291
sherwin <sherwin_sw@163.com>
2022-01-03 22:29:34 +0800

    feat: 内容更新

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <router-view></router-view>
 </template>
 
 <style lang="less">
 #app {
-  margin-top: 60px;
-  color: @main-color;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   -webkit-font-smoothing: antialiased;
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home-page">This is home-page.</div>
+  <div class="home-page">
+    <el-header class="header">
+      <div class="header__title">前端 Vue3 开发脚手架</div>
+      <div class="header__link">
+        <el-link type="info" href="https://github.com/sherwinshen/fe-vue3-startkit" target="_blank" class="github-link">
+          <img src="./../../assets/images/github.png" class="github-logo" />
+          Github
+        </el-link>
+      </div>
+    </el-header>
+    <el-main class="container">
+      <div class="imgs">
+        <img src="./../../assets/images/logos.png" class="full-height" />
+      </div>
+    </el-main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +22,41 @@ export default {
   name: 'HomePage',
 };
 </script>
+
+<style lang="less" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 48px;
+  border-bottom: 1px solid rgba(145, 145, 145, 0.267);
+
+  .github-link {
+    :deep(.el-link--inner) {
+      display: flex;
+      align-items: center;
+    }
+
+    .github-logo {
+      height: 16px;
+      margin-right: 4px;
+    }
+  }
+}
+
+.container {
+  max-width: initial;
+  height: calc(100vh - 48px);
+
+  .imgs {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+  }
+
+  .full-height {
+    height: 100%;
+  }
+}
+</style>
