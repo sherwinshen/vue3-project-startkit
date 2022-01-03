344076efac8a983a439b74bdc6646b80e29fc626
sherwin <sherwin_sw@163.com>
2022-01-03 22:16:42 +0800

    test: 单元测试初体验

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>count is: {{ count }}</p>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const count = ref<number>(0);
+const increment = () => {
+  count.value += 1;
+};
+</script>
